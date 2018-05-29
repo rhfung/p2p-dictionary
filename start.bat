dd7ed057b996fc0c16cb23d8cfe35c85d56d7feb
Richard H Fung <contact@rhfung.com>
2018-05-28 18:30:48 -0700

    quick fix path

--- start.bat
+++ start.bat
@@ -1,1 +1,1 @@
-java -Djava.net.preferIPv4Stack=true -cp ./dist/p2p-dictionary-SNAPSHOT.jar;./lib/*;. com.rhfung.p2pd.Server %*
+java -Djava.net.preferIPv4Stack=true -cp ./dist/p2p-dictionary-SNAPSHOT.jar;./bin/build/lib/*;. com.rhfung.p2pd.Server %*
